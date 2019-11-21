8b7329d7896de6eaaa45d035b87bfd8bb9d79c7e
Vadim <vadim.putrov@gmail.com>
2019-11-21 14:51:44 +0200

    fix working redux devtools

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -2,28 +2,16 @@ import {createStore, applyMiddleware} from "redux"
 import rootReducer from "../reducers"
 import apiSaga from "../sagas"
 import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 const initialiseSagaMiddleware = createSagaMiddleware();
-// declare global {
-//     interface Window {
-//         __REDUX_DEVTOOLS_EXTENSION_COMPOSE__:any
-//     }
-// }
-// declare global {
-//     interface Window {
-//         ['devToolsExtension'] : any;
-//     }
-//   }
 
-// let storeEnhancers: any = window['devToolsExtension'] ? window['devToolsExtension']() : (f:any)=>f;
-// const store: IStore<any> = enhancer(rootReducer, initialState);
-
-//   const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     rootReducer,
-    // storeEnhancers(
+    composeWithDevTools(
         applyMiddleware(initialiseSagaMiddleware)
-    // )
+    )
 )
 
 initialiseSagaMiddleware.run(apiSaga)
